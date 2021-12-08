8f1a757c4dab35d70a5a84d8907f15e3fd9670f0
Yann SUTY <yannsuty20@gmail.com>
2021-12-08 15:00:34 +0100

    Add HTTP Error 417

--- prediction-engine/src/main/java/fr/lernejo/prediction/UnknownCountryException.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/UnknownCountryException.java
@@ -1,5 +1,9 @@
 package fr.lernejo.prediction;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
 public class UnknownCountryException extends RuntimeException {
     public UnknownCountryException(String country) {
         super("Unknown country: " + country);
--- site/src/main/java/fr/lernejo/travelsite/exception/UserNotFoundException.java
+++ site/src/main/java/fr/lernejo/travelsite/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package fr.lernejo.travelsite.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
 public class UserNotFoundException extends RuntimeException {
 }

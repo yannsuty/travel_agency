52cf5e9e67ae210ee29916d7545864f02f944aab
Yann SUTY <yannsuty20@gmail.com>
2021-12-13 23:57:11 +0100

    Jackson Converter

--- site/src/main/java/fr/lernejo/travelsite/Launcher.java
+++ site/src/main/java/fr/lernejo/travelsite/Launcher.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @SpringBootApplication
 public class Launcher {
@@ -16,6 +17,7 @@ public class Launcher {
     PredictionEngineClient predictionEngineClient() {
         Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://localhost:7080/")
+            .addConverterFactory(JacksonConverterFactory.create())
             .build();
 
         return retrofit.create(PredictionEngineClient.class);

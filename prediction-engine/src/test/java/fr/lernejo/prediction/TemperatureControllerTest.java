df291afe813c852b209f0d11f96b3499b09fab49
Yann SUTY <yannsuty20@gmail.com>
2022-01-10 14:12:36 +0100

    test prediction-engine

--- prediction-engine/src/test/java/fr/lernejo/prediction/TemperatureControllerTest.java
+++ prediction-engine/src/test/java/fr/lernejo/prediction/TemperatureControllerTest.java
@@ -2,21 +2,39 @@ package fr.lernejo.prediction;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.junit.jupiter.MockServerExtension;
+import org.mockserver.junit.jupiter.MockServerSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@ExtendWith(MockServerExtension.class)
+@MockServerSettings(ports = {7080, 7081})
 class TemperatureControllerTest {
-    final TemperatureController temperatureController = new TemperatureController();
 
     @Test
-    void getTemperaturePrediction() {
-        TemperaturePrediction temperaturePrediction = temperatureController.getTemperaturePrediction("France");
-        Assertions.assertEquals("France", temperaturePrediction.country());
-        Assertions.assertThrows(UnknownCountryException.class, ()->{
-            temperatureController.getTemperaturePrediction("Fr");
-        });
+    void temperaturePredictionOK(@Autowired MockMvc mockMvc) throws Exception {
+        mockMvc
+            .perform(MockMvcRequestBuilders.get("/api/temperature?country=France"))
+            .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void temperaturePredictionERROR(@Autowired MockMvc mockMvc) throws Exception {
+        mockMvc
+            .perform(MockMvcRequestBuilders.get("/api/temperature?country=Fr"))
+            .andExpect(MockMvcResultMatchers.status().is(417));
     }
 }
